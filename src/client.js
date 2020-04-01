ddaaf72b5fab30b48456b347a25a04b72de73e4c
Bartek Dybowski <b .dybowski@gmail.com>
2020-04-01 21:35:41 +0200

    client side changes

--- src/client.js
+++ src/client.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
 const store = createStore(reducers, { ...window.APP_STATE });
 
 ReactDOM.hydrate(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('app')
 );
