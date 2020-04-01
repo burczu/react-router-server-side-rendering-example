98ffad490d8c8680f0cf7e7beb1951fd2f5352ab
Bartek Dybowski <b .dybowski@gmail.com>
2020-04-01 21:35:12 +0200

    home component created (from the app component)

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Home = ({ initialText, changeText }) => (
+  <div>
+    <p>{initialText}</p>
+    <button onClick={changeText}>change text!</button>
+  </div>
+);
+
+const mapStateToProps = ({ initialText }) => ({
+  initialText,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeText: () => dispatch({ type: 'CHANGE_TEXT' }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
